dc9613f81bd01bbda96050663bb492ab20990894
Rahul Kumar <mailtorahulkrs@gmail.com>
2020-04-17 00:02:53 +0530

    OpenSepratePanel-P1

---
+++ OpenSeparatePanelsAssessment/OpenSeparatePanelsAssessment/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using OpenSeparatePanelsAssessment.ViewModels;
+
+
+namespace OpenSeparatePanelsAssessment
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+ 
+        private void PurpleTalk_Click(object sender, RoutedEventArgs e)
+        {
+            DataContext = new PurPleTalkViewModel();
+        }
+
+        private void XcubeLabs_Click(object sender, RoutedEventArgs e)
+        {
+            DataContext = new XcubeLabsViewModel();
+        }
+
+        private void YesGnome_Click(object sender, RoutedEventArgs e)
+        {
+            DataContext = new YesGnomeViewModel();
+        }
+    }
+}
